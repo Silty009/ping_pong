9f2d77f16271592d71396b947ea5bbb0654b3341
Silty009 <egor47158@gmail.com>
2025-11-06 20:58:17 +0300

    v1
    upload Ping-pong v1: only code without gameplay

---
+++ Ping-pong.py
@@ -0,0 +1,93 @@
+#Создай собственный Шутер!
+
+from pygame import *
+from random import randint
+from time import time as tm
+
+window = display.set_mode((700, 500))
+display.set_caption('Ping-pong')
+background = transform.scale(image.load('Cat_window.jpg'), (700, 500))
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, w, h, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (w, h))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update1(self): #управление игроком
+        keys = key.get_pressed()
+        
+        if keys[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 500 - 80:
+            self.rect.y += self.speed
+    def update2(self):
+        keys = key.get_pressed()
+
+        if keys[K_UP] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 500 - 80:
+            self.rect.y += self.speed
+        
+
+
+
+
+
+
+hero = Player(80, 80, 'Cat (1) (1).png', 80, 80, 2)
+hero2 = Player(80, 80, 'Cat2 (1) (1).png', 80, 80, 2)
+
+
+
+# mixer.init()
+# mixer.music.load('space.ogg')
+# mixer.music.set_volume(0.3)
+# mixer.music.play()
+# fire_ = mixer.Sound('fire.ogg')
+
+
+
+font.init()
+font1 = font.SysFont('Arial', 70)
+win = font1.render('YOU WIN!', True, (225, 215, 0))
+
+
+lose = font1.render('YOU LOSE!', True, (225, 215, 0))
+
+font2 = font.SysFont('Arial', 25) #обязательно заменить класс font на SysFontfont2 = font.SysFont('Arial', 25) 
+
+rel = font2.render('Ждите, перезарядка!', True, (255, 0, 0))
+
+game = True
+clock = time.Clock() #создаем игровой таймер
+
+
+
+
+
+
+
+finish = False
+
+while game:
+    pass 
+    
+
+    
+    
+    
+    
+       
+
+    
+    
+    display.update()
+    clock.tick(60)
+
